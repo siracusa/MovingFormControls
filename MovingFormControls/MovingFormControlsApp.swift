a2b7f115ed11859aa42a3edc4574f0639df4bfe3
John Siracusa <siracusa@gmail.com>
2025-08-21 12:55:21 -0400

    Initial Commit

---
+++ MovingFormControls/MovingFormControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingFormControlsApp.swift
+//  MovingFormControls
+//
+//  Created by Developer on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MovingFormControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
