2c805017945ad7be108a34c5496e33a2b4e3e83e
John Siracusa <siracusa@gmail.com>
2025-10-08 18:44:52 -0400

    Fix improperly curled quotation mark.

--- MovingFormControls/ContentView.swift
+++ MovingFormControls/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                 .frame(width: 48, height: 48)
                 .padding()
 
-            Text("Make sure “Show scroll bars” is set to ”Always” in the “Appearance” section of System Settings, then open the Settings window by clicking the button below.\n\nExpected results: the .fixedSize() Form is not scrollable at all and the toggle controls in the form do not move when the pointer hovers over them.\n\nActual results: the .fixedSize() Form can scroll a pixel or two, and when you hover over the toggle controls, they MOVE and cause the content (and then the window) to resize!")
+            Text("Make sure “Show scroll bars” is set to “Always” in the “Appearance” section of System Settings, then open the Settings window by clicking the button below.\n\nExpected results: the .fixedSize() Form is not scrollable at all and the toggle controls in the form do not move when the pointer hovers over them.\n\nActual results: the .fixedSize() Form can scroll a pixel or two, and when you hover over the toggle controls, they MOVE and cause the content (and then the window) to resize!")
 
             Button("Open Settings") {
                 openSettings()
